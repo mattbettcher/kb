4dbaad9d26317aac7052bb4f522ee403e7a12559
Matt Bettcher <mattbettcher@gmail.com>
2021-07-19 20:46:16 -0400

    Working on basic expressions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#[macro_use]
+extern crate lalrpop_util;
+
+mod ast;
+lalrpop_mod!(pub kb);
+
+use ast::*;
+
+#[test]
+fn expression_test() {
+    let expr = kb::ExprParser::new()
+        .parse("22 * 44 + 66")
+        .unwrap();
+    assert_eq!(&format!("{:?}", expr), "((22 * 44) + 66)");
+}
+
 fn main() {
     println!("Hello, world!");
 }
